27ae72e094e22dfc4b810a478c2d55653d772551
KillerBoy95 <batarkham4@gmail.com>
2026-04-18 12:18:45 -0400

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import path from "path";
 
 const app = express();
-
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.get("/health", (req,res)=>{
@@ -10,6 +11,14 @@ app.get("/health", (req,res)=>{
         message:"Server is healthy"
     });
 });
+
+if(ENV.NODE_ENV == "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("/{*any}",(req,res)=>{
+        res.sendFile(path.join(__dirname, "../frontend","dist", "index.html"));
+    })
+}
 
 app.listen(ENV.PORT,()=>{
     console.log(`App is running on PORT ${ENV.PORT}`);
